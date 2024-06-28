2ff0fffc640aa83fffa5edafc389b5e0dde2e74f
smnvkh <s.masha@me.com>
2024-06-28 11:29:19 +0500

    adaptive

--- javascript/market.js
+++ javascript/market.js
@@ -32,6 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let mainVoodoo = document.querySelector('.block_image')
   mainVoodoo.addEventListener('mouseover', newMainVoodoo)
   mainVoodoo.addEventListener('mouseout', oldMainVoodoo)
+  mainVoodoo.addEventListener('touchstart', newMainVoodoo)
+  mainVoodoo.addEventListener('touchend', oldMainVoodoo)
 
   function newMainVoodoo() {
     document.getElementById('main_image').src = 'images/main_voodoo_colored.png'
